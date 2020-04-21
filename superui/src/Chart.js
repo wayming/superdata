77dda2a802c150dda42ef41451a2703219e0ae89
wayming.z <wayming.z@gmail.com>
2020-04-21 20:18:17 +1000

    use JSON response

--- superui/src/Chart.js
+++ superui/src/Chart.js
@@ -2,37 +2,17 @@ import React from 'react';
 import './App.css';
 import '../node_modules/nvd3/build/nv.d3.css';
 import NVD3Chart from "react-nvd3";
-// import d3 from "d3";
- import { useState } from 'react';
-var datum = [{
-  key: "sun",
-  values: [ 
-    { 'vdate': '2015-06-10T00:00:00.000Z', 'value': 1.18665 },
-    { 'vdate': '2015-06-11T00:00:00.000Z', 'value': 1.19168 },
-    { 'vdate': '2015-06-12T00:00:00.000Z', 'value': 1.18928 },
-    { 'vdate': '2015-06-13T00:00:00.000Z', 'value': 1.18928 },
-    { 'vdate': '2015-06-14T00:00:00.000Z', 'value': 1.18928 },
-    { 'vdate': '2015-06-15T00:00:00.000Z', 'value': 1.18543 },
-    { 'vdate': '2015-06-16T00:00:00.000Z', 'value': 1.18601 },
-    { 'vdate': '2015-06-17T00:00:00.000Z', 'value': 1.19112 },
-    { 'vdate': '2015-06-18T00:00:00.000Z', 'value': 1.18608 },
-    { 'vdate': '2015-06-19T00:00:00.000Z', 'value': 1.19101 },
-    { 'vdate': '2015-06-20T00:00:00.000Z', 'value': 1.19101 },
-    { 'vdate': '2015-06-21T00:00:00.000Z', 'value': 1.19101 },
-    { 'vdate': '2015-06-22T00:00:00.000Z', 'value': 1.19509 },
-    { 'vdate': '2015-06-23T00:00:00.000Z', 'value': 1.20044 },
-  ]
-}];
+import d3 from "d3";
+import { useState } from 'react';
 
 function Chart() {
   const [units, setUnits] = useState(false);
-  console.log(datum)
   fetch('http://localhost:8001')
     .then(response => {
-      return response.text();
+      return response.json();
     })
     .then(data => {
-      console.log(data)
+      // console.log(data)
       setUnits(data);
     });
   return (
@@ -40,14 +20,16 @@ function Chart() {
         {
           React.createElement(NVD3Chart, {
             xAxis: {
-              tickFormat: function(d){ return Date(d); },
+              // tickFormat: function(d){ return Date(d); },
+              tickFormat: function(d){ return d3.time.format('%x')(new Date(d)); },
               axisLabel: 'Period'
             },
             yAxis: {
-              tickFormat: function(d) {return parseFloat(d).toFixed(6); }
+              // tickFormat: function(d) {return parseFloat(d).toFixed(6); }
+              tickFormat: d3.format(',.1%')
             },
             type:'cumulativeLineChart',
-            datum: datum,
+            datum: units  ,
             x: function(d) {
               return new Date(d['vdate']);
             },
@@ -57,7 +39,7 @@ function Chart() {
             height: 600
           })
         }
-      <h1> { units } } </h1>
+      <h1> { JSON.stringify(units) } } </h1>
 
     </div>
   );
